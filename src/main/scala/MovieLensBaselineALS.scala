3e140e11a74a7fe4b2611ed0aef6f9dbbd315572
George Giapitzakis <giorgosgiapis@mail.com>
2024-12-08 20:29:47 -0500

    Added BaseLine and mvn

--- src/main/scala/MovieLensBaselineALS.scala
+++ src/main/scala/MovieLensBaselineALS.scala
@@ -1,11 +1,13 @@
 package ca.uwaterloo.cs651project
 
+import ca.uwaterloo.cs651project.MovieLensZScoreALS.log
 import org.apache.log4j._
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.ALS
 import org.apache.spark.sql.SparkSession
 import org.rogach.scallop._
 
+import java.io.{BufferedWriter, FileWriter}
 import java.nio.file.Paths
 import scala.sys.process._
 
@@ -30,7 +32,6 @@ object MovieLensBaselineALS {
   def main(argv: Array[String]): Unit = {
     val args = new ConfBaseline(argv)
     val spark = SparkSession.builder()
-      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .appName("MovieLensBaselineALS")
       .getOrCreate()
 
@@ -38,17 +39,11 @@ object MovieLensBaselineALS {
     val dataPath = getData(args.size())
 
     val df = spark.read.option("header", "true").option("inferSchema", "true").csv(dataPath)
-    val ratings = df.select("userId", "movieId", "rating")
-    val ratio = if ((args.size() == "small")) {
-      0.8
-    } else {
-      0.9
-    }
-    val rank = if ((args.size() == "small")) {
-      6
-    } else {
-      10
-    }
+    val ratings = df.select("userId", "movieId", "rating").cache()
+    val ratio = if (args.size() == "small") 0.8 else 0.9
+    val rank = if (args.size() == "small") 6 else 10
+    val losses = spark.sparkContext.collectionAccumulator[Double]("Losses")
+
     val ALS = new ALS()
       .setRank(rank)
       .setMaxIter(25)
@@ -59,9 +54,8 @@ object MovieLensBaselineALS {
       .setColdStartStrategy("drop")
 
     for (run <- 1 to args.runs()) {
-      log.info(s"Run $run")
+      log.info(s"ALS Run $run")
       val Array(training, test) = ratings.randomSplit(Array(ratio, 1 - ratio))
-
       val model = ALS.fit(training)
       val predictions = model.transform(test)
 
@@ -70,7 +64,13 @@ object MovieLensBaselineALS {
         .setLabelCol("rating")
         .setPredictionCol("prediction")
       val mse = evaluator.evaluate(predictions)
-      println(s"Run $run MSE = $mse")
+      losses.add(mse)
     }
+    val loss_list = losses.value.toArray
+    log.info("Writing losses to normalized_losses.txt")
+    val filePath = "normalized_losses.txt"
+    val fileWriter = new BufferedWriter(new FileWriter(filePath))
+    fileWriter.write(loss_list.mkString("\n"))
+    fileWriter.close()
   }
 }
